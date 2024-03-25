e689a4c6764c19794e4dd4e872a0c8eced2f5292
Brayan <brianpenalozaortega@gmail.com>
2024-03-24 21:55:27 -0500

    Add configuration files and initial code

---
+++ src/websockets/websocket.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class WebsocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    console.log(data);
+    // this.server.emit('messageServer', 'Hello from server!');
+    // this.server.emit('messageServer', data);
+    client.broadcast.emit('messageServer', data); // broadcast to all clients except sender
+  }
+}
